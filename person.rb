b589bbc489662dd5ebeb5edf3818fa7da250931c
kweeka <mtalbi29@yahoo.com>
2023-01-30 14:52:46 +0100

    Add id attribute

--- person.rb
+++ person.rb
@@ -1,15 +1,16 @@
 class Person
-  attr_reader :parent_permission
+  attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services?
-    of_age? && parent_permission
+    of_age? && @parent_permission
   end
 
   private
