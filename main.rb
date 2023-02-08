d8ac01b0507d34a2f02418ec1a645ce3644eccd1
kweeka <mtalbi29@yahoo.com>
2023-02-08 09:36:08 +0100

    Add ability to load data on app start

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 #! /usr/bin/env ruby
 require_relative 'app'
+require_relative 'serializer'
 
 def main
   puts "Welcome to School Library App!\n\n"
-  app = App.new
+  serializer = Serializer.new
+  books = serializer.load('books')
+  students = serializer.load('students')
+  teachers = serializer.load('teachers')
+  rentals = serializer.load('rentals')
+  people = students.concat(teachers)
+  serializer.apply_rentals(books, people, rentals)
+  app = App.new(serializer, books, people)
   app.run
 end
 
