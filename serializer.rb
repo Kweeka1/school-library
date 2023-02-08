6e434656cffd7afcde89c87abaefc7838008263b
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-02-08 13:21:39 +0100

    Save rentals and fix linter error

--- serializer.rb
+++ serializer.rb
@@ -36,4 +36,25 @@ class Serializer
     serialized_data = JSON.dump(rentals)
     File.write("#{@path}/rentals.json", serialized_data)
   end
+
+  def save(file_name, object)
+    data = []
+    object.each do |obj|
+      current_obj = {}
+      obj.instance_variables.each do |var|
+        current_obj[var.to_s.gsub('@', '')] = obj.instance_variable_get var if var.to_s != '@rentals'
+      end
+      data << current_obj
+    end
+    serialized_data = JSON.dump(data)
+    File.write("#{@path}/#{file_name}.json", serialized_data)
+  end
+
+  def apply_rentals(books, people, rentals)
+    rentals.each do |rental|
+      book = books.find { |current_book| current_book.title == rental['book'] }
+      person = people.find { |current_person| current_person.id == rental['person'] }
+      Rental.new(rental['date'], book, person)
+    end
+  end
 end
